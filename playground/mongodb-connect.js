c916fa5d685a587d3f8aa19f349d8ffa0cdc5346
anubhav <aroraanubhav14@gmail.com>
2017-06-11 01:42:25 +0530

    db connect file updated

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -17,15 +17,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    // db.collection('Users').insertOne({
-    //     name: 'Anubhav Arora',
-    //     age: 23,
-    //     location: 'Delhi'
-    // }, (err, result) => {
-    //     if (err) {
-    //         return console.log('Unable to insert user', err);
-    //     }
-    //     console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
-    // });
+    db.collection('Users').insertOne({
+        name: 'Anubhav Arora',
+        age: 23,
+        location: 'Delhi'
+    }, (err, result) => {
+        if (err) {
+            return console.log('Unable to insert user', err);
+        }
+        console.log(JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2));
+    });
     db.close();
 });
